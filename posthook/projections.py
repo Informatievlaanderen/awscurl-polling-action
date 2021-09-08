e5f26e1bc605ed97544c89d6e4447d79adbadc57
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2021-09-08 14:39:42 +0200

    build: use boto3

--- posthook/projections.py
+++ posthook/projections.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import os, sys, subprocess, time, argparse
+import sys, time, argparse, boto3
 
 # args
 parser = argparse.ArgumentParser(description='projections prehook')
@@ -10,54 +10,24 @@ parser.add_argument('-p','--profile', help='profile name', default='')
 parser.add_argument('-i','--interval', type=int, help='polling interval for fetching desiredCount in seconds. default: 2', default=2)
 args = parser.parse_args()
 
-def exec(cmd):
-    output = subprocess.Popen(cmd,
-                              shell=True,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              universal_newlines=True).communicate()
-    if output[1] != '':
-        print(cmd)
-        print(output[1].strip())
-        sys.exit(-1)
-    return output[0].strip()
-
-def getProfile():
-    if args.profile != '':
-        return f" --profile {args.profile}"
-    return ''
-
-
-def setServiceDesiredCount(count):
-    cmd = f"aws ecs update-service --cluster {args.cluster} --service {args.service}{getProfile()} --desired-count {count}"
-    output = exec(cmd)
-    print(f"update desired count: {output}")
-    if "An error occurred" in output:
-        print(f"update service failed: {output}")
-        sys.exit(-1)
-
-def getServiceDesireCount():
-    cmd = f"aws ecs describe-services --cluster {args.cluster} --service {args.service}{getProfile()} | jq '.services[0].deployments[0].desiredCount'"
-    output = exec(cmd)
-    print(f"current desired count: {output}")
-    return int(output)
+def setServiceDesiredCount(ecs,count):
+    response = ecs.update_service(
+        cluster= args.cluster,
+        service= args.service,
+        desiredCount = count
+    )
 
 
 def main():
-    desiredCount = getServiceDesireCount()        
-    # upscale to 1
-    if desiredCount == 0:
-        print(f'set desired count to 1')
-        setServiceDesiredCount(0)
-        time.sleep(args.interval)
-    
-    # wait till complete
-    while True:
-        desiredCount = getServiceDesireCount()
-        if desiredCount != 0:
-            print(f'current desired count is {desiredCount}')
-            break
-    print(f'posthook executed succesfully')
+    if args.profile != '':
+        session = boto3.Session(profile_name=args.profile)
+        ecs = session.client("ecs")
+    ecs = boto3.client("ecs")
+    print("initialized boto3")
+    print("set desired count to 1")
+
+    setServiceDesiredCount(ecs, 1)
+    print("posthook executed succesfully")
     sys.exit()
 
 if __name__ == "__main__":
--- prehook/projections.py
+++ prehook/projections.py
@@ -1,72 +1,89 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import os, sys, subprocess, time, argparse
+import sys, time, argparse, boto3
 
 # args
 parser = argparse.ArgumentParser(description='projections prehook')
 parser.add_argument('-c','--cluster', help='cluster name', required=True)
 parser.add_argument('-s','--service', help='service name', required=True)
-parser.add_argument('-t','--tablename', help='the tablename projections lock', required=True)
 parser.add_argument('-p','--profile', help='profile name', default='')
 parser.add_argument('-i','--interval', type=int, help='polling interval for fetching desiredCount in seconds. default: 2', default=2)
 args = parser.parse_args()
 
-def exec(cmd):
-    output = subprocess.Popen(cmd,
-                              shell=True,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              universal_newlines=True).communicate()
-    if output[1] != '':
-        print(cmd)
-        print(output[1].strip())
-        sys.exit(-1)
-    return output[0].strip()
+def getDistributionLockName(ecs):
+    # Get task definition name
+    taskDefinitions = ecs.list_task_definitions(
+        familyPrefix='basisregisters-staging-streetname-registry-projections',
+        status='ACTIVE',
+        sort='DESC',
+    )
+    taskDefinitionName = taskDefinitions["taskDefinitionArns"][0]
 
-def getProfile():
-    if args.profile != '':
-        return f" --profile {args.profile}"
-    return ''
+    # Get distribution lock names
+    description = ecs.describe_task_definition(
+        taskDefinition= taskDefinitionName
+    )
 
+    # Distributedlocks
+    distributedLocks = set()
+    for containers in description["taskDefinition"]["containerDefinitions"]:
+        an_iterator = filter(lambda env : env["name"] == "DistributedLock__TableName", containers["environment"])
+        for i in list(an_iterator):
+            distributedLocks.add(i["value"])
+    
+    return list(distributedLocks)    
 
-def setServiceDesiredCount(count):
-    cmd = f"aws ecs update-service --cluster {args.cluster} --service {args.service}{getProfile()} --desired-count {count}"
-    output = exec(cmd)
-    print(f"update desired count: {output}")
-    if "An error occurred" in output:
-        print(f"update service failed: {output}")
-        sys.exit(-1)
+def setServiceDesiredCount(ecs,count):
+    response = ecs.update_service(
+        cluster= args.cluster,
+        service= args.service,
+        desiredCount = count
+    )
 
-def getServiceDesireCount():
-    cmd = f"aws ecs describe-services --cluster {args.cluster} --service {args.service}{getProfile()} | jq '.services[0].deployments[0].desiredCount'"
-    output = exec(cmd)
-    print(f"current desired count: {output}")
-    return int(output)
+def isDownScaled(ecs):
+    response = ecs.describe_services(
+        cluster = args.cluster,
+        services=[
+            args.service,
+        ]
+    )
+    runningCount = response["services"][0]["runningCount"]
+    pendingCount = response["services"][0]["pendingCount"]
+    return runningCount == 0 and pendingCount == 0
 
-def deleteLockDynomoDb():
-    cmd = f"aws dynamodb delete-table --table-name {args.tablename}{getProfile()}"
-    print(f"deleting lock: {output}")
-    output = exec(cmd)
-    return int(output)
-
+def deleteLockDynomoDb(ecs, dynamodb):
+    locks = getDistributionLockName(ecs)
+    for lock in locks:
+        print(f'deleting table: {lock}')
+        response = dynamodb.delete_table(
+            TableName = lock
+        )
+        print(f'deleted table: {lock}')
 
 def main():
+    if args.profile != '':
+        session = boto3.Session(profile_name=args.profile)
+        ecs = session.client("ecs")
+        dynamodb = session.client("dynamodb")
+    ecs = boto3.client("ecs")
+    dynamodb = boto3.client("dynamodb")
+    print("initialized boto3")
+
     # downscale to 0
-    desiredCount = getServiceDesireCount()
-    if desiredCount > 0:
-        print(f'set desired count to 0')
-        setServiceDesiredCount(0)
+    if not isDownScaled(ecs):
+        print("set desired count to 0")
+        setServiceDesiredCount(ecs, 0)
         
     # wait till complete
-    while True:
+    while not isDownScaled(ecs):
+        print("sleep")
         time.sleep(args.interval)
-        if desiredCount == 0:
-            print(f'current desired count is {desiredCount}')
-            break
+    print("service is downscaled")
     
     # delete lock
-    deleteLockDynomoDb()
-    print(f'prehook executed succesfully')
+    deleteLockDynomoDb(ecs, dynamodb)
+    print("prehook executed succesfully")
+
     sys.exit()
 
 if __name__ == "__main__":
