45fc52ded67ec68da72d690d8c41a8bed7a86d8c
Koen Metsu <koen@kometsoft.be>
2024-09-26 14:14:07 +0200

    feat: post for status

--- polling-action/main.py
+++ polling-action/main.py
@@ -59,7 +59,7 @@ def sendBuildRequest():
 
 def getStatus(build_id):
     aws_status_url = f'{args.status_url}/{build_id}'
-    cmd = f"awscurl --access_key '{args.access_key}' --secret_key '{args.secret_key}' --region '{args.region}' --service execute-api {aws_status_url}"
+    cmd = f"awscurl --access_key '{args.access_key}' --secret_key '{args.secret_key}' --region '{args.region}' --service execute-api -X POST {aws_status_url}"
     output = exec(cmd)
     status_responses.append(output)
     return json.loads(output)
