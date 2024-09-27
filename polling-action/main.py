4c92701a0eabd66be2afa25c528cb1b52fc850e8
Koen Metsu <koen@kometsoft.be>
2024-09-27 14:06:29 +0200

    feat: add moar logging

--- polling-action/main.py
+++ polling-action/main.py
@@ -62,8 +62,11 @@ def getStatus(build_id):
     aws_status_url = f'{args.status_url}/{build_id}'
     print(f'Calling {aws_status_url}')
     cmd = f"awscurl --access_key '{args.access_key}' --secret_key '{args.secret_key}' --region '{args.region}' --service execute-api -X POST {aws_status_url}"
+    print(f'Calling cmd {cmd}')
     output = exec(cmd)
+    print(f'Output: {output}')
     status_responses.append(output)
+    print(f'Loading json')
     return json.loads(output)
 
 def main():
